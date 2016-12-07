a7139840e722e1ebd4e949f9748330a088acebe9
h0r0man <roman.horokhovatskyy@gmail.com>
2016-12-07 17:53:15 +0100

    Updated index with default home layout

--- index.php
+++ index.php
@@ -3,14 +3,23 @@
 	<main class="index">
 
     <?php if (get_the_archive_title()): ?>
-      <h2><?php single_cat_title(); ?></h2>
+      <h2 class="hidden"><?php single_cat_title(); ?></h2>
     <?php endif; ?>
 
 		<?php if (have_posts()): ?>
 
-			<?php while (have_posts()): the_post(); ?>
-				<?php get_template_part('template-parts/content', 'preview'); ?>
-			<?php endwhile; ?>
+      <div class="grid grid--three">
+        <div class="grid__items">
+
+          <div class="grid__sizer"></div>
+          <div class="grid__gutter"></div>
+
+    			<?php while (have_posts()): the_post(); ?>
+    				<?php get_template_part('template-parts/content', 'preview'); ?>
+    			<?php endwhile; ?>
+
+        </div>
+      </div>
 
 		<?php else: ?>
 
