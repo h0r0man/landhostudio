57b73956877c244826c99003c3c02b73fea8d3cc
h0r0man <roman.horokhovatskyy@gmail.com>
2016-12-09 15:16:11 +0100

    Added static privacy policy to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 		<footer role="contentinfo" class="footer">
-			<p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php esc_html_e('All rights reserved', 'horoman'); ?>.</p>
+			<p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php esc_html_e('All rights reserved', 'horoman'); ?>. <a href="//www.iubenda.com/privacy-policy/7970188" target="_blank" rel="nofollow"><?php esc_html_e('Privacy Policy', 'horoman'); ?></a>.</p>
 		</footer>
 		<?php wp_footer(); ?>
 	</body>
