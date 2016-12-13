ebad7c68aa83b29780968c22fa3465575ec58fda
h0r0man <roman.horokhovatskyy@gmail.com>
2016-12-13 13:24:55 +0100

    Revert "Added critical css [alpha]"
    This reverts commit 2e814386bea023855ef37c17c1665c12b0f4e7b0.

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
 		<footer role="contentinfo" class="footer">
 			<p>&copy; 2014 – <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. <?php esc_html_e('All rights reserved', 'horoman'); ?>. <a href="//www.iubenda.com/privacy-policy/7970188" target="_blank" rel="nofollow"><?php esc_html_e('Privacy Policy', 'horoman'); ?></a>.</p>
 		</footer>
-		<?php if (is_preview()): ?>
-			<span class="preview"><?php esc_html_e('Preview', 'horoman'); ?></span>
-		<?php endif; ?>
 		<?php wp_footer(); ?>
     <?php if (get_field('options_analytics', option)): ?>
 			<script>
