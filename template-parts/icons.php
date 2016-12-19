c83d0df1b2d54b3ef30a58493c6497540f1cbed1
h0r0man <roman.horokhovatskyy@gmail.com>
2016-12-19 18:41:36 +0100

    Added icons

--- template-parts/icons.php
+++ template-parts/icons.php
@@ -1,15 +1,6 @@
-<!-- Windows -->
-<meta name="application-name" content="<?php bloginfo('name'); ?>">
-<meta name="msapplication-square70x70logo" content="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/win/small.jpg">
-<meta name="msapplication-square150x150logo" content="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/win/medium.jpg">
-<meta name="msapplication-wide310x150logo" content="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/win/wide.jpg">
-<meta name="msapplication-square310x310logo" content="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/win/large.jpg">
-<meta name="msapplication-TileColor" content="">
-
-<!-- iOS -->
-<link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/ios/touch-icon-60.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/ios/touch-icon-76.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/ios/touch-icon-120.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/ios/touch-icon-152.png">
-
-<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/favicon.ico">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/manifest.json">
+<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/safari-pinned-tab.svg" color="#f6ada1">
+<meta name="theme-color" content="#f6ada1">
